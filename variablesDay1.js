2aa45bff72ef6480b40b24aa9cb29e109f794325
kishanpaneri <kishan.paneri123@gmail.com>
2024-07-14 14:46:27 +0530

    first day commit

---
+++ variablesDay1.js
@@ -0,0 +1,32 @@
+var num = 5;
+console.log(num);
+
+let str = "kishan";
+console.log(str);
+
+const isUser = false;
+console.log(isUser);
+
+var num1 = 5;
+console.log(num1);
+console.log(typeof num1);
+var str1 = "kishan";
+console.log(str1);
+console.log(typeof str1);
+var isLoggedIn = true;
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+var obj = { name: "kishan", age: 30 };
+console.log(obj);
+console.log(typeof obj);
+var arr = [1, 2, 3];
+console.log(arr);
+console.log(typeof arr);
+
+let test1 = "kishan";
+console.log(test1);
+test1 = "kishan paneri";
+console.log(test1);
+
+const tester = "lokesh";
+tester = "yogesh";
